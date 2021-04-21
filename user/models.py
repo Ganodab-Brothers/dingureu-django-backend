b8a22d4526da36f7d44c20b77200e88d242366e5
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-21 20:20:27 +0900

    feat(user): School 모델 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    school_code = models.CharField(max_length=20, null=False)
+    school_name = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.school_name
