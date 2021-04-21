f6bd8ebdcd99ed93aa21c75d9038d5f437c06c19
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-21 09:20:12 +0900

    Initial commit

---
+++ config/envs.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 't6p&4yojr&qu$8-@exk#(ptj9agmo6@+_1x5xjzs4_latwayhg')
+
+DEBUG = os.environ.get("DEBUG", "true")
+
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "1234")
+DB_NAME = os.environ.get("DB_NAME", "database")
+
+WAS_HOST = os.environ.get(
+    "WAS_HOST") if not DEBUG else "http://localhost:8000/api/"
