329810aaebea361a58d68ba85989856ea0824f18
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-22 08:20:33 +0900

    feat(user): UserView 추가
    - 회원가입
    - 프로필 조회

--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http.request import HttpRequest
+from user.serializer import UserSerializer
+from rest_framework import permissions, status, viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from user.models import User
+from user.serializer import UserSerializerDocument
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+
+class UserView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+
+    @swagger_auto_schema(responses={
+        201: UserSerializerDocument,
+        400: "Bad Request",
+    })
+    @action(detail=False, methods=['post'])
+    def register(self, request: HttpRequest, *args, **kwargs):
+        serializer: UserSerializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # todo: get my profile
+    # todo: update profile
+    # todo: set password
