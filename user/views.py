9d1e5163c0727e02674bc7d061563eac2f66fafc
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-24 17:43:36 +0900

    feat(user): 변경된 serializers의 파일명에 맞게 views.py 수정

--- user/views.py
+++ user/views.py
@@ -1,10 +1,10 @@
 from django.http.request import HttpRequest
-from user.serializer import UserSerializer
+from user.serializers import UserSerializer
 from rest_framework import permissions, status, viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from user.models import User
-from user.serializer import UserSerializerDocument
+from user.serializers import UserSerializerDocument
 from drf_yasg.utils import swagger_auto_schema
 
 
