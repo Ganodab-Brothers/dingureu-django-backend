27cdb13922d786afce43978203be36b6e4aa41bb
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-24 17:41:49 +0900

    feat(user): 사용자 모델 관련 내용 변경
    - school 모델에 location 필드 추가
    - user 모델에 is_active 추가
    - RegisterApplication 모델 추가

--- user/models.py
+++ user/models.py
@@ -7,26 +7,31 @@ from django.utils.translation import ugettext_lazy as _
 class School(models.Model):
     school_code = models.CharField(max_length=20, null=False)
     school_name = models.CharField(max_length=30, null=False)
-    location = models.CharField(max_length=10, null=False)
+    location = models.CharField(max_length=10, null=False, default="nowhere")
 
     def __str__(self):
         return self.school_name
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password, nickname=None):
+    def create_user(self, username, password, nickname=None, school=None):
         if not username:
             raise ValueError(_('Users must have an username'))
         user = self.model(username=username,
                           password=password,
-                          nickname=nickname)
+                          nickname=nickname,
+                          school=school)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password, nickname):
-        user = self.create_user(username, password, nickname)
+    def create_superuser(self, username, password, nickname="admin"):
+        school: School = School.objects.get_or_create(
+            school_code="-1", school_name="admin school",
+            location="nowhere")[0]
+        user = self.create_user(username, password, nickname, school)
         user.is_superuser = True
+        user.is_active = True
         user.save(using=self._db)
         return user
 
@@ -44,15 +49,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=14, null=True)
     gender = models.CharField(
         max_length=1,
-                              choices=GENDER_CHOICES,
-                              null=False,
+        choices=GENDER_CHOICES,
+        null=False,
         default="M",
     )
     birthday = models.DateField(null=True)
     student_id = models.CharField(max_length=8, null=True)
     school: School = models.ForeignKey(
         School,
-                                       on_delete=models.PROTECT,
+        on_delete=models.PROTECT,
         null=False,
     )
     date_joined = models.DateTimeField(default=timezone.now)
@@ -70,3 +75,16 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def is_staff(self):
         return self.is_superuser
+
+
+class RegisterApplication(models.Model):
+    user: User = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    school_id_card_url = models.TextField(null=False)
+
+    def __str__(self):
+        school_name = self.user.school.school_name
+        username = self.user.username
+        return f"{school_name} | {username}"
