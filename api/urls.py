53472a4801f6714df96f2d1f28497cd493e288f3
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-24 17:50:14 +0900

    feat(api): Article 앱의 두개의 라우터 추가
    - articles/school
    - articles/local

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,21 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from user.views import UserView
 from file.views import FileView
+from article.views import SchoolArticleView, LocalArticleView
 
 router = DefaultRouter()
 router.register(r'users', UserView, basename="user")
 router.register(r'files', FileView, basename="file")
+router.register(
+    r'articles/school',
+    SchoolArticleView,
+    basename="school_article",
+)
+router.register(
+    r'articles/local',
+    LocalArticleView,
+    basename="local_article",
+)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view()),
