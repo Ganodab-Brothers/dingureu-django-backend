6009dfaa6e52129c84d8f3f75a3388d1f1d1b139
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-24 17:46:28 +0900

    feat(user): 어드민 패널에 새 모델 등록

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from user.models import User
+from user.models import User, School, RegisterApplication
 
-admin.site.register(User)
+admin.site.register(RegisterApplication)
+admin.site.register(School)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    class RegisterApplicationInline(admin.TabularInline):
+        model = RegisterApplication
+
+    inlines = [RegisterApplicationInline]
