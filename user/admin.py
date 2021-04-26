9a3d3763b043ef6a8da643e10e308aa6e338c89c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-26 15:13:39 +0900

    fix(user): Admin에서 유저 생성 시 password가 해싱되지 않는 문제 수정

--- user/admin.py
+++ user/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from user.models import User, School, RegisterApplication
+from user.forms import UserCreationForm
 
 admin.site.register(RegisterApplication)
 admin.site.register(School)
 
 
-@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    form = UserCreationForm
+
     class RegisterApplicationInline(admin.TabularInline):
         model = RegisterApplication
 
     inlines = [RegisterApplicationInline]
+
+
+admin.site.register(User, UserAdmin)
