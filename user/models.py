6602d455e3a8dad0e7ff0cf6c9320c963500789e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-26 15:44:57 +0900

    fix(user): location 필드가 없는 문제 해결

--- user/models.py
+++ user/models.py
@@ -59,6 +59,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         on_delete=models.PROTECT,
         null=False,
     )
+    location = models.CharField(max_length=10, null=False)
     date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
