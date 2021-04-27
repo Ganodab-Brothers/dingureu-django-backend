f76b14c4c4cb554707793698d0d43ab4b5da8557
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-04-27 12:16:45 +0900

    feat(article): hearts 필드 추가

--- article/models.py
+++ article/models.py
@@ -8,11 +8,13 @@ class SchoolArticle(models.Model):
         AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
+        related_name='writer',
     )
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50, null=False)
     content = models.TextField(null=False)
     school = models.ForeignKey(School, on_delete=models.PROTECT, null=False)
+    hearts = models.ManyToManyField(AUTH_USER_MODEL, related_name='hearts')
 
     def __str__(self):
         return self.title
